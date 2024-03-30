d2aa7217bc46d1c88fd4f3e3edc4ef2af91941c8
kkumarM <karthikkumaar.m@gmail.com>
2024-03-30 22:40:44 +0530

    modified login file

--- login/run.py
+++ login/run.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3 
 
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/login",methods=['GET'])
+@app.route("/login",methods=['POST','GET'])
 def login():
-    #data = get
-    return "hello World"
+    data = request.get_json()
+    print("data", data) 
+    return "Login Successfull"
 
-#if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port= 5000, debug= True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port= 5000, debug= True)
 
 
